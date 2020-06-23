22eabc22a80e36747c86d8a8745d5bce63515857
battula nagaanjaneyulu <39210416+nagaanjaneyulubattula@users.noreply.github.com>
2020-06-23 12:16:30 +0530

    Hello World

--- JavaBasics1/src/JavaBasics1/Hello_World.java
+++ JavaBasics1/src/JavaBasics1/Hello_World.java
@@ -1,9 +1,9 @@
-
+package JavaBasics1;
 public class Hello_World {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println("Hello World!!");
 	}
 
 }
