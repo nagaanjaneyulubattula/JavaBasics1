b1f9d8c7a2d42ee5354325017804b6906cc126d0
battula nagaanjaneyulu <39210416+nagaanjaneyulubattula@users.noreply.github.com>
2020-06-23 21:05:34 +0530

    Arrays

---
+++ JavaBasics1/src/Arrays/Multi_Dimensional_Array.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class Multi_Dimensional_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[ ][ ] = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}};
+		for(int i=0;i<3;i++)
+		{
+			for (int j=0;j<5;j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ JavaBasics1/src/Arrays/Single_Dimensional_Array.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class Single_Dimensional_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]=new int[5];
+		for(int x: a)
+		{
+			System.out.println(x);
+		}
+	}
+
+}
