dbf18f259c8edff26955d22962273f7186a0b8f1
battula nagaanjaneyulu <39210416+nagaanjaneyulubattula@users.noreply.github.com>
2020-06-23 13:48:12 +0530

    static and initializer block

---
+++ Static and Initializer Block/src/static_initializer/main.java
@@ -0,0 +1,21 @@
+package static_initializer;
+public class main {
+	static
+	{
+		System.out.println("Welcome to JavaProgramming");
+		System.out.println("This is static block Example");
+
+	}
+	public static void one()
+	{
+		System.out.println("Welcome to JavaProgramming");
+		System.out.println("This is Initializer block Example");
+
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		one();
+		System.out.println("This is main method");
+	}
+}
